4c442069832af5e375ceae8176133a1cb05a140b
tuananhhdev <138957065+tuananhhdev@users.noreply.github.com>
2023-09-05 00:16:39 +0700

    Add files via upload
    nop bai

---
+++ Capstone_Admin/Cyber_Phone/admin/index.js
@@ -0,0 +1,6 @@
+/*var modal = document.getElementById("modal");
+var closeModal = document.getElementById("close");
+
+function closeModal(close) {
+  document.getElementById("close").style.display = "none";
+}*/
